5da4568148459a3d4e291f3062fe27db5b8f5918
ThunderDrake <56897482+ThunderDrake@users.noreply.github.com>
2022-11-07 17:37:12 +0000

    Deploying to gh-pages from @ ThunderDrake/Park-cafe-new@865542b5b10932e5129076c8549251c4240dc31a 🚀

---
+++ telegram.php
@@ -0,0 +1,26 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$guests = $_POST['guests'];
+$zone = $_POST['zone'];
+$date = $_POST['date'];
+$token = "5728801940:AAHA16SVayz6WdeNMCL9X1zskbAdZ_Qb_5A";
+$chat_id = "-837008055";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Кол-во гостей' => $guests,
+  'Зона посадки:' => $zone,
+  'Дата банкета:' => $date
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+?>
